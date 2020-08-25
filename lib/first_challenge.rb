42a8c1c3dc5090edb4e9909dca66ea4a8e60df6c
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-25 02:19:46 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -10,8 +10,25 @@ def first_challenge
          :age => "54"
        },
        :hero => {
+         :name => "Romeo",
+         :age => "15"
+       },
+       :hero_friends => [
+         {
+           :name => "Benvolio",
+           :age => "17",
+           :attitude => "worried"
+         },
+         {
+           :name => "Mercutio",
+           :age => "18",
+           :attitude => "hot-headed"
+         }
+        ]
+      },
          
-       }
+         
+         ]
        
 
      },
