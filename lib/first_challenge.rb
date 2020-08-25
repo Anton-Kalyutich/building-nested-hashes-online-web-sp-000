2f7a459e1270c3bd45d4c277ff98c58af5e4f6c2
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-25 02:23:33 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -50,7 +50,7 @@ def first_challenge
          {
            :name => "Nurse",
            :age => "44",
-           :attitude => "hot-headed"
+           :attitude => "worried"
          }
         ]
       }
